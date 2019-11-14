abc32acfec1a77210b1f93db586726db087b0faa
Elizabeth Kipp <liz@elizabethkipp.com>
2019-11-14 12:48:52 -0600

    changed env.PORT back

--- index.js
+++ index.js
@@ -293,7 +293,7 @@ app.get('/movies/director/:Name', passport.authenticate('jwt', {
 });
 
 // Listen for requests
-var port = process.env.MONGO_URL
+var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function() {
-  console.log("listening on Port 3000");
+  console.log("Listening on Port 3000");
 });
