3ea66b7d68285af61ae88bbc2239c5edca60212e
Elizabeth Kipp <liz@elizabethkipp.com>
2019-11-20 14:39:11 -0600

    Added MainView component

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -19,4 +19,36 @@ class MainView extends React.Component {
       <div className="main-view"></div>
     );
   }
+} // Closes class MainView extends
+
+export class MainView extends React.Component {
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios.get('https://liz-flix.herokuapp.com/movies')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    // If the state isn't initialized, this will throw on runtime before the data is initially loaded
+    const { movies } = this.state;
+
+    //Before the movies have been loaded
+    if (!movies) return <div className="main-view" />;
+
+    return (
+      <div className="main-view">
+        {movies.map(movie => (
+          <div className="movie-card" key={movie._id}>{movie.Title}</div>
+        ))}
+      </div>
+    );
+  }
 }
